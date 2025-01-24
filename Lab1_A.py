4b08e4a1a498121ef9bb4a2d22ddbaa0b6388734
Lomin3s <60679584+Lomin3s@users.noreply.github.com>
2025-01-23 19:50:59 -0500

    Update Lab1_A.py

--- Lab1_A.py
+++ Lab1_A.py
@@ -4,6 +4,6 @@ def main():
 
     fahrenheit = (celsius * 9/5) + 32
 
-    print(f'That is {fahrenheit:.1f} degrees in Fahrenheit!')
+    print(f'That is {fahrenheit:.1f} degrees Fahrenheit!')
 
 main()
